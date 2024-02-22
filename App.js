b880496a0695b22218c1a8853812ad5cdd884adc
TilakAzmeera <tilakazmeera@gmail.com>
2024-02-22 14:17:37 +0530

    Ajio website

---
+++ App.js
@@ -0,0 +1,36 @@
+
+// var mainImg=document.getElementById('mainImg');
+// var smallimg=document.getElementsByClassName('small-img');
+
+// smallimg[0].onclick=function(){
+//   mainImg.src=smallimg[0].src;
+// }
+// smallimg[1].onclick=function(){
+//   mainImg.src=smallimg[1].src;
+// }
+// smallimg[2].onclick=function(){
+//   mainImg.src=smallimg[2].src;
+// }
+// smallimg[3].onclick=function(){
+//   mainImg.src=smallimg[3].src;
+// }
+
+
+// var hamburger=document.querySelector(".hamburger");
+// var navmenu=document.querySelector(".navmenu");
+
+// hamburger.addEventListener("click",()=>{
+//   hamburger.classList.toggle("active");
+//   navmenu.classList.toggle("active");
+// })
+
+
+
+console.log("welcome");
+
+
+
+
+
+
+ 
